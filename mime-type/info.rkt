e81bc2c587fe6741fcec7110217f7b2be9e2f03f
Bogdan Popa <bogdan@defn.io>
2022-08-23 19:38:53 +0300

    build: use implies instead of update-implies

--- mime-type/info.rkt
+++ mime-type/info.rkt
@@ -2,10 +2,14 @@
 
 (define collection 'multi)
 (define version "1.0")
-(define deps '("base"))
-(define build-deps '("base"
-                     "mime-type-lib"
-                     "racket-doc"
-                     "rackunit-lib"
-                     "scribble-lib"))
-(define update-implies '("mime-type-lib"))
+(define deps
+  '("base"
+    "mime-type-lib"))
+(define build-deps
+  '("base"
+    "mime-type-lib"
+    "racket-doc"
+    "rackunit-lib"
+    "scribble-lib"))
+(define implies
+  '("mime-type-lib"))
