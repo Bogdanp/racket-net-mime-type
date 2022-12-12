961742e5517ddf0cde130f0a18ca532a9734e16e
Bogdan Popa <bogdan@defn.io>
2022-12-12 10:07:54 +0200

    build: add license to info.rkt files

--- mime-type-lib/info.rkt
+++ mime-type-lib/info.rkt
@@ -1,5 +1,6 @@
 #lang info
 
+(define license 'BSD-3-Clause)
 (define collection "net")
 (define version "1.0")
 (define deps '("base"))
--- mime-type/info.rkt
+++ mime-type/info.rkt
@@ -1,5 +1,6 @@
 #lang info
 
+(define license 'BSD-3-Clause)
 (define collection 'multi)
 (define version "1.0")
 (define deps
